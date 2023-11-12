a8bc6891ab606db8e497e0526c1f8a8ab68d95e5
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-12 20:40:15 +0600

    Union and Intersection types

---
+++ Module-01/src/1.10.ts
@@ -0,0 +1,39 @@
+{
+  // -------------Union and Intersection types--------- //
+  // Union type
+  //   type FrontedDeveloper = "fakibazDeveloper" | "juniorDeveloper";
+  //   type FullStackDeveloper = "FrontedDeveloper" | "FullStackDeveloper";
+  //   type Developer = FrontedDeveloper | FullStackDeveloper;
+  //   const newDeveloper: FrontedDeveloper = "fakibazDeveloper";
+  //   type User = {
+  //     name: string;
+  //     email?: string;
+  //     gender: "male" | "female";
+  //     blood: "O+" | "O-" | "A+" | "A-";
+  //   };
+  //   const user1: User = {
+  //     name: "rahat",
+  //     gender: "male",
+  //     blood: "O+",
+  //   };
+
+  // Intersection types
+
+  type FrontedDeveloper = {
+    skills: string[];
+    designation1: "FrontedDeveloper";
+  };
+
+  type BackedDeveloper = {
+    skills: string[];
+    designation2: "BackedDeveloper";
+  };
+
+  type FullStackDeveloper = FrontedDeveloper & BackedDeveloper;
+
+  const fullStackDeveloper: FullStackDeveloper = {
+    skills: ["html", "css", "Express"],
+    designation1: "FrontedDeveloper",
+    designation2: "BackedDeveloper",
+  };
+}
