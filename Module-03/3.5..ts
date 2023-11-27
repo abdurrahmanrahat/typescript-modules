979a20c31f0c77a66f73e0f3f139d293ca846590
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2023-11-27 06:49:36 +0600

    Access modifiers

---
+++ Module-03/3.5..ts
@@ -0,0 +1,38 @@
+{
+  // Access modifiers
+
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    protected _balance: number; // change can be possible for this claass, not outside
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    // function for updating balance
+    addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  // make instance using BankAccount class
+  const goribManush = new BankAccount(189, "Rahat Hossain", 34000);
+  goribManush.addDeposit(5);
+  goribManush.getBalance();
+
+  // child account via extends BankAccount //
+  class StudentAccount extends BankAccount {
+    test() {
+      this._balance;
+    }
+  }
+
+  //
+}
